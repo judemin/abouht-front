a5a60539a5ee2f25a454f45f2049f3b8c223132e
sterdsterd <sterdsterd@icloud.com>
2023-07-03 04:31:02 +0900

    Feat: Add Service Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Landing from "./pages/Landing"
 import Team from "./pages/Team"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Service from "./components/Service"
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/team" element={<Team />} />
+          <Route path="/service" element={<Service />} />
         </Routes>
       </div>
       <Footer />
---
+++ src/components/Service.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+type Props = {}
+
+const Service = (props: Props) => {
+  return <div>서비스 소개가 들어가는 페이지입니다.</div>
+}
+
+export default Service
