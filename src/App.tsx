33b1d880f979b0f3eacf6102773f35e630fcd095
sterdsterd <sterdsterd@icloud.com>
2023-07-03 01:41:31 +0900

    Feat: Add Navigation Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Landing from "./pages/Landing"
+import NavigationBar from "./components/NavigationBar"
 
 const App = () => {
   return (
     <BrowserRouter>
+      <NavigationBar />
       <div className="container">
         <Routes>
           <Route path="/" element={<Landing />} />
